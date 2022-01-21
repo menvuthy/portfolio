882aef710b379e216a460b52752d9a0365dd4990
GeoSpatialyst <geopy.course@gmail.com>
2022-01-21 14:16:53 +0900

    ud

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,7 @@ html_theme = 'sphinx_book_theme'
 html_logo = 'img/men-vuthy-logo.jpg'
 html_title = ""
 
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
